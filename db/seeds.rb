f3a00e848b80db2a234b64df3fb40520f5684e92
Sohail Karim <sohailkarim66@gmail.com>
2024-11-08 21:36:00 +0500

    uncomment the seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ require_relative 'seeds/states_seeder'
 require_relative 'seeds/cities_seeder'
 require_relative 'seeds/users_seeder'
 
-# seed_continents
-# seed_countries
-# seed_states
-# seed_cities
+seed_continents
+seed_countries
+seed_states
+seed_cities
 seed_users
